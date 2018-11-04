3243995f70b4916d5077b3e97c25c32bbf0c22c0
reddog335 <williamward@Williams-MacBook-Pro.local>
2018-11-04 13:06:20 -0600

    Initial Commit

---
+++ src/main/java/com/springboot/demo/springinfivesteps/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.springinfivesteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
